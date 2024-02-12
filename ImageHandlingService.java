6e935009a89d112d730eae35b52faf38d339e462
Manthansankhe <109619638+Manthansankhe@users.noreply.github.com>
2024-02-12 14:18:01 +0530

    Add files via upload
    Sevices for image handling

---
+++ ImageHandlingService.java
@@ -0,0 +1,12 @@
+package com.aap.service;
+
+import java.io.IOException;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.app.dto.ApiResponse;
+
+public interface ImageHandlingService {
+    ApiResponse uploadImage(Long customerId, MultipartFile image) throws IOException;
+    byte[] downloadImage(Long customerId) throws IOException;
+}
