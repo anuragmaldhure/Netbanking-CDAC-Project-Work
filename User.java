5a1c15fc0d14befc2f25b37e3be1b0441fc3b5d3
Ganesh Yadav <128254310+Ganesh5623@users.noreply.github.com>
2024-02-21 00:45:42 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,45 @@
+package com.app.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @Column(name = "username", unique = true)
+    protected String username;
+
+    @Column(name = "password")
+    protected String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
